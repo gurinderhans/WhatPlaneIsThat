9e7e1161b48f22888b3b2f09f7d15a1673426155
Gurinder Hans <hello@gurinderhans.me>
2015-08-03 23:21:24 -0700

    add map, get user location on map load

--- app/windows/WhatPlaneIsThat/WhatPlaneIsThat/MainPage.xaml.cs
+++ app/windows/WhatPlaneIsThat/WhatPlaneIsThat/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Devices.Geolocation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,24 @@ namespace WhatPlaneIsThat
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Map_Loaded(object sender, RoutedEventArgs e)
+        {
+            // get user location
+            var locator = new Geolocator() { DesiredAccuracy = PositionAccuracy.High };
+            var userLocation = await locator.GetGeopositionAsync();
+
+            var pin = new MapIcon()
+            {
+                Location = userLocation.Coordinate.Point,
+                Title = "You are here!",
+                NormalizedAnchorPoint = new Point() { X = 0.5, Y = 0.5 },
+            };
+
+            Map.MapElements.Add(pin);
+
+            await Map.TrySetViewAsync(userLocation.Coordinate.Point, 17, 0, 0, MapAnimationKind.Bow);
         }
     }
 }
