1288b224acd0d785caa3af3362ea80388618302e
Gurinder Hans <hello@gurinderhans.me>
2015-08-04 21:21:09 -0700

    fetch planes and plot on map

---
+++ app/windows/WhatPlaneIsThat/WhatPlaneIsThat/GeoLocation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace WhatPlaneIsThat
+{
+    class GeoLocation
+    {
+        private GeoLocation()
+        {
+            // empty constructor
+        }
+
+        private static double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        private static double RadianToDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
+        /**
+         * Calculates LatLng of some point at a distance from given latitude, longitude at an angle
+         * @param location - given location
+         * @param bearing  - give bearing / angle (in degrees)
+         * @param distance - distance in Km
+         * @return - new LatLng that is distance away from current point at some angle
+        */
+        public static Geopoint boundingBox(Geocoordinate location, double bearing, double distance)
+        {
+
+            float radius = 6378.1f;
+            double latitude = location.Latitude;
+            double longitude = location.Longitude;
+
+            // new latitude
+            double nLat = RadianToDegree(Math.Asin(Math.Sin(DegreeToRadian(latitude)) * Math.Cos(distance / radius) + Math.Cos(DegreeToRadian(latitude)) * Math.Sin(distance / radius) * Math.Cos(DegreeToRadian(bearing))));
+            double nLng = RadianToDegree(DegreeToRadian(longitude) + Math.Atan2(Math.Sin(DegreeToRadian(bearing)) * Math.Sin(distance / radius) * Math.Cos(DegreeToRadian(latitude)), Math.Cos(distance / radius) - Math.Sin(DegreeToRadian(latitude)) * Math.Sin(DegreeToRadian(nLat))));
+
+            return new Geopoint(new BasicGeoposition() { Latitude = nLat, Longitude = nLng });
+        }
+
+    }
+}
--- app/windows/WhatPlaneIsThat/WhatPlaneIsThat/MainPage.xaml.cs
+++ app/windows/WhatPlaneIsThat/WhatPlaneIsThat/MainPage.xaml.cs
@@ -14,6 +14,12 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Controls.Maps;
 using Windows.Devices.Geolocation;
+using System.Net;
+using System.Diagnostics;
+using Windows.Data.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -38,13 +44,75 @@ namespace WhatPlaneIsThat
             var pin = new MapIcon()
             {
                 Location = userLocation.Coordinate.Point,
-                Title = "You are here!",
+                Title = "You",
                 NormalizedAnchorPoint = new Point() { X = 0.5, Y = 0.5 },
             };
 
             Map.MapElements.Add(pin);
 
-            await Map.TrySetViewAsync(userLocation.Coordinate.Point, 17, 0, 0, MapAnimationKind.Bow);
+            var nw = GeoLocation.boundingBox(userLocation.Coordinate, 315, 100).Position;
+            var se = GeoLocation.boundingBox(userLocation.Coordinate, 135, 100).Position;
+
+            // request plane data
+            var BASE_URL = "http://lhr.data.fr24.com/zones/fcgi/feed.js";
+            var OPTIONS_FORMAT = String.Format("?bounds={0},{1},{2},{3}&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=900&gliders=1&stats=1&", 
+                nw.Latitude, se.Latitude, nw.Longitude, se.Longitude);
+
+            JsonObject data = await GetAsync(BASE_URL + OPTIONS_FORMAT);
+            plotPlaneData(data);
+
+            // zoom map to user location
+            await Map.TrySetViewAsync(userLocation.Coordinate.Point, 12, 0, 0, MapAnimationKind.Bow);
+
+        }
+
+        public async Task<JsonObject> GetAsync(string uri)
+        {
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync(uri);
+            return await Task.Run(()=> JsonObject.Parse(content));
+        }
+
+        public void plotPlaneData(JsonObject jsonObj)
+        {
+            try
+            {
+
+                foreach (string key in jsonObj.Keys)
+                {
+                    try
+                    {
+                        // data type array is plane data type
+                        JsonArray plane = jsonObj[key].GetArray();
+
+                        // plot the plane on map
+                        var latitude = plane[1].GetNumber();
+                        var longitude = plane[2].GetNumber();
+
+                        var planeGeoPoint = new Geopoint(new BasicGeoposition() { Latitude = latitude, Longitude = longitude });
+
+                        Debug.WriteLine(planeGeoPoint.Position.Latitude + " " + planeGeoPoint.Position.Longitude);
+
+                        var pin = new MapIcon()
+                        {
+                            Location = planeGeoPoint,
+                            Title = "Plane",
+                        };
+                        Map.MapElements.Add(pin);
+
+                    }
+                    catch (Exception e)
+                    {
+                        // Exception can be ignored as we do not care about data that we can't conver to type 
+                    }
+                }
+
+            }
+            catch (WebException e)
+            {
+                Debug.WriteLine("No plane data recieved.");
+                return;
+            }
         }
     }
 }
